e0881b67d44153da03d19e6b4b3089288397715f
MozartCalado <mozartcalado@gmail.com>
2024-11-24 17:57:57 -0300

    Update tcad.php
    Foi colocado o PHP dentro do body do HTML

--- proj/tcad.php
+++ proj/tcad.php
@@ -1,3 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/tcad.css"/>
+    <title>Página de Cadastro</title>
+    <link rel="shortcut icon" href="IMG/icon-aliens.ico" type="image/x-icon">
+</head>
+<body>
+    
 <?php
 // Conexão com o banco de dados (MySQL)
 $servername = "localhost";  // Servidor do banco de dados
@@ -67,17 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
-
-<!DOCTYPE HTML>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/tcad.css"/>
-    <title>Página de Cadastro</title>
-    <link rel="shortcut icon" href="IMG/icon-aliens.ico" type="image/x-icon">
-</head>
-<body>
+    
     <header>
     </header>
     <main>
